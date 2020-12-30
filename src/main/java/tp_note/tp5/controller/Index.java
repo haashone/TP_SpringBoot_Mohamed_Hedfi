9606ac2a131cf263ce14f1a963f059a654a545c4
haashone <hachone10@gmail.com>
2020-12-30 03:22:02 +0100

    updating

--- src/main/java/tp_note/tp5/controller/Index.java
+++ src/main/java/tp_note/tp5/controller/Index.java
@@ -3,7 +3,7 @@ package tp_note.tp5.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class Index {
--- src/main/java/tp_note/tp5/model/AddressRepository.java
+++ src/main/java/tp_note/tp5/model/AddressRepository.java
@@ -1,8 +1,6 @@
 package tp_note.tp5.model;
 
-import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
